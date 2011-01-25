02905d1934a84cfc470c34b100c7415d6e652822
Lucie Meresse <lumu59@gmail.com>
2011-01-25 22:45:51 +0100

    Modifications et optimisations des classes : on_touch_down() on_touch_move() on_touch_up() Ajout de commentaires pour faciliter la comprehension du code

--- Sprouts.py
+++ Sprouts.py
@@ -3,6 +3,9 @@
 
 #__all__ = ('Point', 'Tracer')
 
+
+#Tous les imports utiles et nécessaires pour le bon déroulement du programme
+
 from kivy.app import App
 from kivy.factory import Factory
 from kivy.lang import Builder
@@ -11,53 +14,111 @@ from kivy.uix.widget import Widget
 from kivy.uix.scatter import ScatterPlane
 from kivy.graphics import Color, Ellipse
 from random import random, randint
-from kivy.properties import ListProperty, BooleanProperty
+from kivy.properties import ListProperty, BooleanProperty, NumericProperty, ObjectProperty
+from kivy.graphics.vertex_instructions import Line
 
-#Classe representant un point, defini sa taille, couleur, position
+#DEBUT du code
+
 class Point(Widget):
-    # Constructeur du noeud, il appelle son père widget
+    '''Classe Point(), possède la propriété degré'''
+
     def __init__(self, **kwargs):
+        '''Constructeur du noeud, il appelle son père widget'''
+
         super(Point, self).__init__(**kwargs)
-       
+
+    #Propriété degré, par défaut égal à 0 attribué a chaque création de point
+    degre = NumericProperty(0)
 
 
-#Classe pour le tracer de ligne entre les Points
 class Tracer(Widget):
+    '''Classe Tracer() gère l'affichage des lignes graphiquement
+    possède 3 méthodes : down(), move() et up()
+    '''
 
     def __init__(self, **kwargs):
+        '''Constructeur de la classe Tracer() + declaration d'une variable
+           ligne qui par la suite prendra la création de ligne
+        '''
         super(Tracer, self).__init__(**kwargs)
         self.ligne = None
 
     def on_touch_down(self, touch):
-        print touch.pos
-        print "je suis là"
-        if self.collide_point(touch.x, touch.y):
-            print("toucher")
-            self.ligne = Ligne(points = [touch.x, touch.y])
+        '''Methode on_touch_down() : quand on down un event
+        # -recupère tout ses freres, verifie isinstance()
+        # -gère la detection d'un clic dans un point
+        # -vérifie que le point n'est pas saturé en degré (degre<4)
+        # -créer et attache la ligne à Tracer
+        '''
+        root = self.parent
+
+        for child in root.children:
+            if not isinstance(child, Point):
+                continue
+            if not child.collide_point(touch.x, touch.y):
+                continue
+            if child.degre > 2:
+                continue
+            print child, 'je suis touché, argh je me meurs trop sa mère'
+            self.ligne = Ligne(points=[touch.x, touch.y], valid=True, first=child)
             self.add_widget(self.ligne)
-            return True
-        #TO DO : collide point
+            break
+
 
     def on_touch_move(self, touch):
-        #des que je bouge, cette fonction est appellee (= boucle)
-        print touch.pos
+        '''methode on_touch_move(): quand on move un event
+           si tout est ok dans on_touch_down(), on trace
+        '''
         if self.ligne:
-            self.ligne.points = self.ligne.points + [touch.x, touch.y] 
-        #if self.collide_point(touch.x, touch.y):
-            #self.pos = (touch.pos)
-            #return True
-        return False
+            self.ligne.points = self.ligne.points + [touch.x, touch.y]
 
+
+       
     def on_touch_up(self, touch):
-        self.ligne = None
-        print touch.pos
+        '''methode on_touch_up() = quand on up un event
+        # -recupère tout ses freres, verifie isinstance()
+        # -gère la detection de fin de ligne dans un point
+        # -vérifie que le point de fin n'est pas saturé en degre (degre<4)
+        # -si tout est ok, on increment degre dans le point de départ(first) et celui
+        # d'arrivée(x)...(dans first on stocke une instance de point())
+        # sinon : on remove à partir de if ! isinstance() la ligne de tracer()
+        '''
+        root = self.parent
 
+        if not self.ligne:
+            return
+
+        self.ligne.valid = False
+
+        for child in root.children:
+            if not isinstance(child, Point):
+                continue
+            if not child.collide_point(touch.x, touch.y):
+                continue
+            if child.degre > 2:
+                continue
+            if self.ligne.first == child:
+                continue
+            print 'point arrivée'
+            self.ligne.first.degre += 1
+            child.degre += 1
+            print 'degre first' , self.ligne.first.degre
+            print 'degre point :', child.degre 
+            self.ligne.valid = True
+            break
+
+        if self.ligne.valid is False:
+            self.remove_widget(self.ligne)
+
+        #remise à None : pour recommencer une ligne de "zero"
+        self.ligne = None    
 
 class Ligne(Widget):
     #declaration d'une nouvelle proporiété
     points = ListProperty([])
     valid = BooleanProperty(True)
-
+    first = ObjectProperty
+    
 
 class PointApp(App):
     def build(self):
@@ -65,18 +126,17 @@ class PointApp(App):
         # w, h = canvas.size(); 
         root = Widget()
         #root = ScatterPlane()
-        w, h = Window.size        
+        w, h = Window.size
         #on ajoute la liste des positions des Point que l'on crées à la liste
         #root
-        
-        for i in xrange(10):  
-            point = Point(size=(7, 7), pos =(random()*w, random()*h))
+
+        for i in xrange(10):
+            point = Point(size=(10, 10), pos =(random()*w, random()*h))
             root.add_widget(point)
         root.add_widget(Tracer())
         return root
-        
+
 
 if __name__ == '__main__':
     PointApp().run()
-                    
-     
+
