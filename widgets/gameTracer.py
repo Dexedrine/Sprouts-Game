afa18ada5dbb0a9bc447f6d838088b886e028cc1
Lucie Meresse <lumu59@gmail.com>
2011-02-06 14:24:27 +0100

    Ajout de la fonction intersect trouve sur internet pour detecter si il y a une intersection entre deux segments

--- widgets/gameTracer.py
+++ widgets/gameTracer.py
@@ -3,15 +3,31 @@ from kivy.uix.widget import Widget
 from widgets.gamepoint import Point
 from math import sqrt
 from widgets.gameLigne import Ligne
+from kivy.vector import Vector
+
+
+
+'''Algo d'intersection trouvé sur internet :
+    http://www.bryceboe.com/2006/10/23/line-segment-intersection-algorithm/'''
+def ccw(A,B,C):
+	return (C[1]-A[1])*(B[0]-A[0]) > (B[1]-A[1])*(C[0]-A[0])
+
+def intersect(A,B,C,D):
+        return ccw(A,C,D) != ccw(B,C,D) and ccw(A,B,C) != ccw(A,B,D)
+
+
+
+
+
 
 class Tracer(Widget):
     '''Classe Tracer() gère l'affichage des lignes graphiquement
     possède 3 méthodes : down(), move() et up()
     '''
-    
+
     def __init__(self, **kwargs):
         '''Constructeur de la classe Tracer() + declaration d'une variable
-           ligne qui par la suite prendra la création de ligne
+        ligne qui par la suite prendra la création de ligne
         '''
         super(Tracer, self).__init__(**kwargs)
         self.ligne = None
@@ -24,7 +40,7 @@ class Tracer(Widget):
         # -gère la detection d'un clic dans un point
         # -vérifie que le point n'est pas saturé en degré (degre<4)
         # -créer et attache la ligne à Tracer
-      
+
         '''
         root = self.parent
 
@@ -48,16 +64,16 @@ class Tracer(Widget):
         - si tout est ok dans on_touch_down(), on trace
         - calcul en live des x et y minimaux / maximaux afin de tracer les bbox
         - calcul de la longueur courante de la ligne     racine carrée de ((x2-x1) * (x2-x1) + ((y2-y1)*(y2-y1))
-        
+
         '''
         #on creer la variable precedentTouch si il n'y a pas encore eu de tracer de ligne
         if self.ligne:
-                      
+
             self.ligne.points = self.ligne.points + [touch.x, touch.y]
             self.ligne.longueur = self.ligne.longueur + sqrt((touch.x - self.ligne.xprec) * (touch.x - self.ligne.xprec) + (touch.y - self.ligne.yprec) * (touch.y - self.ligne.yprec))
             print 'longueur courante = ' ,self.ligne.longueur
             self.ligne.xprec = touch.x
-	    self.ligne.yprec = touch.y
+            self.ligne.yprec = touch.y
             if self.ligne.minx > touch.x:
                 self.ligne.minx = touch.x
             if self.ligne.miny > touch.y:
@@ -66,9 +82,7 @@ class Tracer(Widget):
                 self.ligne.maxx = touch.x
             if self.ligne.maxy < touch.y:
                 self.ligne.maxy = touch.y
-            
-          
-         
+
     def on_touch_up(self, touch):
         '''methode on_touch_up() = quand on up un event
         # -recupère tout ses freres, verifie isinstance()
@@ -79,7 +93,7 @@ class Tracer(Widget):
         # d'arrivée(x)...(dans first on stocke une instance de point())
         # sinon : on remove à partir de if ! isinstance() la ligne de tracer()
         '''
-        
+
         root = self.parent
 
         if not self.ligne:
@@ -93,47 +107,78 @@ class Tracer(Widget):
             if not child.collide_point(touch.x, touch.y):
                 continue
             if self.ligne.first == child:
-            	if child.degre > 1:
-            		continue
+                if child.degre > 1:
+                    continue
             if child.degre > 2:
-               	continue
+                continue
             print 'point arrivée'
             self.ligne.first.degre += 1
             child.degre += 1
             print 'degre first' , self.ligne.first.degre
             print 'degre point :', child.degre 
             self.ligne.valid = True
-            
-            
-            ''' ICI on doit faire la creation du nouveau POINT ! 
-            	- on regarde la taille et on l'a divise par deux pour trouver le milieu de la ligne
-            	- on place au coordonnée indiquer par le nombre obtenu le nouveau point (?) 
+
+
+            ''' Test de l'intersection entre deux ensembles de points
             '''
-      
-            precx = self.ligne.points[0]
-            precy = self.ligne.points[1]
-            cx = None # n*2
-            cy = None # n*2 +1
-            for i in range(len(self.ligne.points) / 2):
-            	cx = self.ligne.points[2*i]
-            	cy = self.ligne.points[2*i +1]
-            	if cx == precx or cx == precy :
-            		continue
-            	if self.ligne.milieu > self.ligne.longueur / 2:
-            		print 'le milieu trouvé est ' , self.ligne.milieu , ' avec une longueur initiale de : ' , self.ligne.longueur
-            		break
-             	self.ligne.milieu += sqrt((cx - precx) * (cx - precx) + (cy - precy) * (cy - precy))
-            	precx = cx
-            	precy = cy
-            pointMilieu = Point(size=(25, 25),
-                              pos =(precx, precy))
-          #      PointApp.listPoint.append(pointMilieu)
-            root.add_widget(pointMilieu)
-           
+
+            #inclure le test de la Bbox ICI
+
+            l1 = self.ligne
+            for l2 in self.children: #je recupere ttes mes lignes
+                if l1 is l2: continue
+                if self.is_intersect(l1.points, l2.points):
+                    print 'intersection entre' ,l1,l2
+                    self.ligne.valid = False
+                                   
+
+            ''' ICI on doit faire la creation du nouveau POINT ! 
+            - on regarde la taille et on l'a divise par deux pour trouver le milieu de la ligne
+            - on place au coordonnée indiquer par le nombre obtenu le nouveau point (?) 
+            '''
+            if self.ligne.valid is True: 
+                precx = self.ligne.points[0]
+                precy = self.ligne.points[1]
+                cx = None # n*2
+                cy = None # n*2 +1
+                for i in range(len(self.ligne.points) / 2):
+                    cx = self.ligne.points[2*i]
+                    cy = self.ligne.points[2*i +1]
+                    if cx == precx or cx == precy :
+                        continue
+                    if self.ligne.milieu > self.ligne.longueur / 2:
+                        print 'le milieu trouvé est ' , self.ligne.milieu , ' avec une longueur initiale de : ' , self.ligne.longueur
+                        break
+                    self.ligne.milieu += sqrt((cx - precx) * (cx - precx)
+                                          + (cy - precy) * (cy - precy))
+                    precx = cx
+                    precy = cy
+                pointMilieu = Point(size=(25, 25),
+                                    pos =(precx, precy))
+                #      PointApp.listPoint.append(pointMilieu)
+                root.add_widget(pointMilieu)
+
 
         if self.ligne.valid is False:
             self.remove_widget(self.ligne)
 
         #remise à None : pour recommencer une ligne de "zero"
         self.ligne = None    
+
+
+
+    def is_intersect(self, l1, l2):#on passe deux ensembles de points = ligne
+        for i in xrange(0, len(l1)-4, 2):
+            xp = l1[i]
+            yp = l1[i+1]
+            xc = l1[i+2]
+            yc = l1[i+3]
+            for j in xrange(0, len(l2)-4, 2):
+                xpb = l2[j]
+                ypb = l2[j+1]
+                xcb = l2[j+2]
+                ycb = l2[j+3]
+                if intersect((xp, yp), (xc, yc),(xpb, ypb), (xcb, ycb)):
+                    return True
+
 
